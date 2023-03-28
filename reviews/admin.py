f713ca3a62e7f5d916685d18dbb0cb41c309c976
yamesyung <simionut@gmail.com>
2023-03-28 22:30:17 +0300

    changes to django admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('title', 'isbn')
+    list_filter = ('publisher', 'publication_date',)
+    search_fields = ('title', 'isbn__exact', 'publisher__name',)
+
+
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookContributor)
+admin.site.register(Review)
